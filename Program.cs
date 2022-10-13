b776436dc3f4e3e9d201074d73bc919717107e27
jayukasode23 <jayukasode2398@gmail.com>
2022-10-13 13:01:04 +0530

    Added Line Comparison Program

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using LineComparison2Program;
+
+LineLenght obj = new LineLenght();
+obj.CheckLineLenght();
+
+LineEquality obj1 = new LineEquality();
+obj1.CheckLineEquality();
+
+CompareLines ob2 = new CompareLines();
+ob2.CheckCompareLines();
+
+LineEqualAndCompare line = new LineEqualAndCompare();
+line.CheckLineEqualAndCompare();
